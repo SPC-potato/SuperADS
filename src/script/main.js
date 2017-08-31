6d93762785bf98d257c7cc55306009b2161b139d
Pan <540650060@qq.com>
2017-08-31 10:52:58 +0800

    webpack上传

---
+++ src/script/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by 舍予 on 2017/8/31.
+ */
+function helloworld() {
+    alert("hello,world")
+}
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 舍予 on 2017/8/31.
+ */
+
+module.exports = {
+    entry: './src/script/main.js',
+    output:{
+        path: '/dist/js',
+        filename: 'bundle.js'
+    }
+
+}
